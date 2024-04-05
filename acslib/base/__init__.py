12809968dd2b8fc15ee68e2a2a0ce55d0593a66a
Luc <lgsanche@ncsu.edu>
2024-04-05 12:26:10 -0400

    revert import sorting causing circular imports

--- acslib/base/__init__.py
+++ acslib/base/__init__.py
@@ -1,5 +1,4 @@
-import acslib.base.status as status
-
-from .acs import AccessControlSystem
 from .config import ACSConfig, ACSConfigException
 from .connection import ACSConnection, ACSRequestData, ACSRequestException, ACSRequestResponse
+from .acs import AccessControlSystem
+import acslib.base.status as status
